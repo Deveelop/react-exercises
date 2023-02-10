f127c2882d36ebbc482805feca1b9135512e15ce
vicson <vicsonabuka@gmail.com>
2023-02-10 17:56:58 +0100

    Add the input file

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react'; 
 import Todo  from './Todo';
 import './App.css';
 
@@ -7,8 +8,23 @@ function App() {
     'Practice React',
     'Profit from React'
   ];
+  const [enteredValue, setEnteredValue] = useState('')
+  const clickHandler= (e)=>{
+    setEnteredValue(e.target.value)
+  }
+  const values = {
+    value: enteredValue
+  }
+  console.log(values);
   return (
     <div>
+      <div className='task'>
+      <form>
+        <label>Todo</label>
+        <input onChange={clickHandler}/>
+        <button>Create</button>
+      </form>
+      </div>
      <ul>
       Todo: output todos
       <Todo text={Dummy_Todos} />
