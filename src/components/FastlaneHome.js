e152cee57cca8dfb86f47ff5abfd2aaf4fe0e67d
vicson <vicsonabuka@gmail.com>
2023-05-12 17:21:38 +0100

    Add making button for login functional

--- src/components/FastlaneHome.js
+++ src/components/FastlaneHome.js
@@ -1,14 +1,14 @@
 import React from "react";
 import styles from './Fastlane.module.css'
 import Button from "../UI/Button";
-export default function FastlaneHome(){
+export default function FastlaneHome(props){
     return(
         <>
         <div className={styles.panel}>
            <img src='/images/stylinimg.jpg'/> 
             <h2>Viclane</h2>
             
-            <Button>Login</Button>
+            <Button onClick={props.onLogin}>Login</Button>
              <Button>Sign Up</Button>
             
             
