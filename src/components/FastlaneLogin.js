57a0e6ae933280e4758e81a567b63bc623ba5df7
vicson <vicsonabuka@gmail.com>
2023-05-12 17:22:30 +0100

    Added more update here

--- src/components/FastlaneLogin.js
+++ src/components/FastlaneLogin.js
@@ -2,16 +2,16 @@ import React,{useState} from "react";
 import styles from './Login.module.css'
 import ErrorModal from "../UI/ErrorModal";
 import Button from "../UI/Button";
+import {FaEnvelope, FaEye, FaEyeSlash, FaUnlock} from 'react-icons/fa'
 
 export default function FastlaneLogin(props){
 const [isError, setError] = useState();
 const [email, setEmail] = useState('')
 const [pass, setPass] = useState('')
+const [isVisible, setVisible] = useState(false);
 let realpass= 'vic123'
 let realmail= 'vicsonabuka@gmail.com'
-const clickHandle = () => {
-   
-}
+
 const emailChanger = (e) => {
 setEmail(e.target.value)
 
@@ -40,25 +40,47 @@ const submitter = (e) => {
     }
     console.log(email, pass)
 }
+const errorChecked = () => {
+setError(null);
+}
+
+let password = document.querySelector('#password');
+const checkPass = () => {
+ if(password.type == 'password') {
+    password.type = 'text'
+     setVisible(true);
+ }
+}
+const checkPassed = () => {
+    if(password.type == 'text'){
+       password.type = 'password'
+        setVisible(false);
+    }
+}
     return(
         <>
-       { isError && <ErrorModal title={isError.title} message={isError.message}/>}
+       { isError && <ErrorModal title={isError.title} message={isError.message} onError={errorChecked}/>}
         <div className={styles.container}>
             <img src="/images/stylinimg.jpg"/>
             <div className={styles.form}>
                 <h2>Login</h2>
                 <form onSubmit={submitter}>
                 <div className={styles.form_control}>
+                    <div className={styles.ic1}><FaEnvelope/></div>
                     <label>Email</label>
                     <input type="Email" placeholder="Email" onChange={emailChanger}/>
                 </div>
                 <div className={styles.form_control}>
+                   
+                    <div className={styles.eyes}>{!isVisible && <FaEye onClick={checkPass}/>}
+                    {isVisible && <FaEyeSlash onClick={checkPassed}/>}
+                    </div>
                     <label>Password</label>
-                    <input type="password" placeholder="Enter password" onChange={passChanger}/>
+                    <input id="password" type="password" placeholder="Enter password" onChange={passChanger}/>
                 </div>
                 <div className={styles.linked}><a id="forgotpass" href="#">Forgot Password?</a>
                 </div>
-                <Button onClick={clickHandle}>Login</Button>
+                <Button >Login</Button>
             </form>
             <div className={styles.linkedo}><label>Don't have account ? <a href="#">Sign Up</a></label></div>
         </div>
